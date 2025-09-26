3197e67aefd08963e5a77aba0bb8f3d78332d324
Kristin Ahnlund <kristin.ahnlund@gmail.com>
2025-09-26 13:20:57 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    internal class Circle
+    {
+        public Circle(int radius) 
+        {
+            Radius = radius;
+        }
+
+
+        
+        
+        public int Radius { get; set; }
+        
+
+        public static double GetArea(int radius)
+        {
+            return radius * radius * Math.PI;
+        }
+
+        public static double GetCircumference(int radius)
+        {
+            return 2 * radius * Math.PI;
+        }
+
+        public static double GetVolume(int radius)
+        {
+            return (4 * Math.PI *  (radius ^ 3)) / 3;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Lab_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int radius = 1;
+            bool isRunning = true;
+
+            while (isRunning) 
+            {
+                Console.Write("Hej! Skriv in radien av din cirkel: ");
+
+                radius = int.Parse(Console.ReadLine());
+                if (radius > 0)
+                {
+                    Console.WriteLine($"Arean av en cirkel med radie {radius} är {Circle.GetArea(radius)}.");
+
+                    Console.WriteLine($"Omkretsen av en cirkel med radie {radius} är {Circle.GetCircumference(radius)}.");
+
+                    Console.WriteLine($"Volymen av en sfär med radien {radius} är {Circle.GetVolume(radius)}.");
+                }
+                else { isRunning = false; }
+            }
+        }
+    }
+}
